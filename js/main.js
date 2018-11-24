4d50709fb6128fb31bdafe61f6f64edbbb701d4b
Joanna Gammel <jgammel@risd.edu>
2018-11-24 12:49:31 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var mic;
 var fft;
-var div = document.getElementById('output');
-
 
 function setup() {
+  createCanvas(710, 200);
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -13,40 +12,65 @@ function setup() {
   // By default, it does not .connect() (to the computer speakers)
   mic.start();
   fft.setInput(mic);
+}
+
+// function draw() {
+//   background(200);
+
+//   // Get the overall volume (between 0 and 1.0)
+//   var vol = mic.getLevel();
+//   fill(127);
+//   stroke(0);
+
+//   // Draw an ellipse with height based on volume
+//   var h = map(vol, 0, 1, height, 0);
+//   ellipse(width/2, h - 25, 50, 50);
+// }
+
+function draw() {
+   background(200);
+
+   var spectrum = fft.analyze();
+
+   beginShape();
   
-}
-
-var spectrum = fft.analyze();
-
-//STATEMENT STARTS HERE//
+   for (i = 0; i<spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0) );
      
-if(spectrum[i] > 0){
-  switch(i % 7){
-    case 0:
-      div.innerHTML += 'red' + '<br/>';
-      break;
-    case 1:
-      div.innerHTML += 'orange' + '<br/>';
-       break;
-    case 2:
-      div.innerHTML += 'yellow' + '<br/>';
-      break;
-    case 3:
-      div.innerHTML += 'green' + '<br/>';
-      break;
-    case 4:
-      div.innerHTML += 'blue' + '<br/>';
-      break;
-    case 5:
-      div.innerHTML += 'violet' + '<br/>';
-      break;
-    case 6:
-      div.innerHTML += 'pink' + '<br/>';
-      break;
-    default:
-      console.log("unknown tone");
+     //STATEMENT STARTS HERE//
      
-  }
+     if(spectrum[i] > 0){
+      switch(i % 7){
+        case 0:
+          console.log("red");
+          break;
+        case 1:
+          console.log("orange");
+           break;
+        case 2:
+          console.log("yellow");
+          break;
+        case 3:
+          console.log("green");
+          break;
+        case 4:
+          console.log("blue");
+          break;
+        case 5:
+          console.log("violet");
+          break;
+        case 6:
+          console.log("pink");
+          break;
+        default:
+          console.log("unknown tone");
+         
+      }
+     }
+     
+     //STATEMENT ENDS HERE//
+     
+   }
+  
+   endShape();
 }
-
-//STATEMENT ENDS HERE//
